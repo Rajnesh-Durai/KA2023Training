ffadc735858c637700e48e75a6a19cea5981fd53
Rajnesh Durai R T <127297822+Rajnesh-kanini@users.noreply.github.com>
2023-07-06 19:26:24 +0530

    Add files via upload

---
+++ C#/DBConnection/DBConnect.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DBConnection
+{
+    internal class DBConnect
+    {
+        SqlConnection conn;
+        public void OpenConnec(string cnnstr)
+        {
+            conn = new SqlConnection(cnnstr);
+            conn.Open();
+            Console.WriteLine("Opened");
+        }
+        public void CreateTable()
+        {
+            
+            SqlCommand cmd = new SqlCommand("create table stud_table1(rollno int, stud_name nvarchar(20))", conn);
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Table Created");
+            
+        }
+        public void InsertTable() 
+        {
+            SqlCommand cmd = new SqlCommand("insert into stud_table1 values(104,'Laxmeesh')", conn);
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Values Inserted");
+        }
+        public void UpdateTable() 
+        {
+            SqlCommand cmd = new SqlCommand("update stud_table1 set stud_name='Rajesh' where rollno=102", conn);
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Values Updated");
+        }
+        public void ReadTable() 
+        {
+            SqlCommand cmd = new SqlCommand("select * from stud_table1", conn);
+            SqlDataReader sdr= cmd.ExecuteReader();
+            if(sdr.HasRows)
+            {
+                while (sdr.Read())
+                {
+                    Console.WriteLine(sdr["rollno"] + " " + sdr["stud_name"]);
+                }
+                Console.WriteLine("Table Readed");
+            }
+            else
+            {
+                Console.WriteLine("The Table is empty...");
+            }
+        }
+        public void CloseConnec()
+        {
+            conn.Close();
+        }
+       
+    }
+}
---
+++ C#/DBConnection/Program.cs
@@ -0,0 +1,40 @@
+﻿using DBConnection;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Text;
+class Program : ConfigurationSection
+{
+    public static void Main(String[] args)
+    {
+        //CONFIGURATION  ==> .NET CORE
+        Console.WriteLine("--------");
+        string s1 = ConfigurationManager.AppSettings["num1"];
+        Console.WriteLine(s1);
+        Console.WriteLine(ConfigurationManager.AppSettings["num2"]);
+
+
+
+        /*
+         
+        DYNAMIC WAY OF CONNECTING DB FROM USER
+         
+        StringBuilder sb = new StringBuilder();
+        Console.WriteLine("Enter the datasource= ");
+        sb.Append(Console.ReadLine());
+        Console.WriteLine(";database=");
+        sb.Append(Console.ReadLine());
+        Console.WriteLine(";integrated security=SSPI;");
+
+        Console.WriteLine(sb);
+
+        DBConnect DB = new DBConnect();
+        DB.OpenConnec(sb.ToString());
+
+
+        //DB.InsertTable();
+        //DB.UpdateTable();
+        DB.ReadTable();
+        DB.CloseConnec();
+        */
+    }
+}
