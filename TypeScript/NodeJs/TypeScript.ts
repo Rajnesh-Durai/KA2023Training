8f28edfd735efeb6fb9d30107bce17942962f6b9
Rajnesh-kanini <rajnesh.kanini@gmail.com>
2023-07-07 11:30:03 +0530

    commit

--- TypeScript/NodeJs/TypeScript.ts
+++ TypeScript/NodeJs/TypeScript.ts
@@ -1,5 +1,43 @@
-const f1=():number=>{
-    var b=27
-    return b+18;
+
+
+ console.log("hello");
+ function ans(name:string)
+ {
+    var output;
+    if(name==="Raja")
+    {
+        output=name;
+    }
+    else
+    {
+        output="incorrect";
+    }
+    console.log(output);
+ }
+ ans("Raja");
+
+//let ==> FOR
+
+//  for(let i:number=0;i<5;i++)
+//  {
+//     setTimeout(function(){console.log(i)},5000);
+//  }
+
+const fun1= function(){
+    return 25;
 }
-console.log(f1())
+console.log(fun1())
+
+const fun2=()=> 19;
+console.log(fun2())
+
+
+//object and this keyword
+
+const obj1={
+    name:'Raja',
+    fun3:function(){
+        setTimeout(()=>{ console.log(this.name)},1000) 
+    }
+}
+obj1.fun3();
