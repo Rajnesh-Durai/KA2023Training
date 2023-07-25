a6380dc9aa04700407fdd760528bd54621eb8a25
Rajnesh Durai R T <127297822+Rajnesh-kanini@users.noreply.github.com>
2023-07-25 15:18:04 +0530

    Add files via upload

---
+++ React/Redux/src/App.js
@@ -0,0 +1,13 @@
+import CounterComponent from "./counter/CounterComponent";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <CounterComponent/>
+    </div>
+  );
+}
+
+export default App;
